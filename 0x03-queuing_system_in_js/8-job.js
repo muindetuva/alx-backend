cb977ddfc5ae7bf2320f5d5119df16780b048f7b
Alfred Tuva <muindetuva@gmail.com>
2025-05-22 21:48:10 +0300

    Add reusable function

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,25 @@
+export default function createPushNotificationsJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
+
+  for (const jobData of jobs) {
+    const job = queue.create('push_notification_code_3', jobData).save((err) => {
+      if (!err) {
+        console.log(`Notification job created: ${job.id}`);
+      }
+    });
+
+    job.on('complete', () => {
+      console.log(`Notification job ${job.id} completed`);
+    });
+
+    job.on('failed', (err) => {
+      console.log(`Notification job ${job.id} failed: ${err}`);
+    });
+
+    job.on('progress', (percent) => {
+      console.log(`Notification job ${job.id} ${percent}% complete`);
+    });
+  }
+}

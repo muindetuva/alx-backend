964e7b5822d158f9008506097f0c5b807618642f
Alfred Tuva <muindetuva@gmail.com>
2025-05-22 14:09:46 +0300

    Add task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """App configs for i18n"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+babel = Babel(app)
+
+
+@app.route('/')
+def index() -> str:
+    """Render the index page"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
